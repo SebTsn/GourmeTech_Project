4eeabdd17f7061415ebf9d781823d6192f167039
SebTsn <sebastien.toussaint.pro@outlook.fr>
2025-04-16 14:57:07 +0200

    maj index 2.0.0 + darkmode localStorage

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,41 @@
 // Dark Mode 
 
-function darkMode() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-     // Change l’emoji selon le thème
-     darkModeButton.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
-  }
+// function darkMode() {
+//     let element = document.body;
+//     element.classList.toggle("dark-mode");
+//      // Change l’emoji selon le thème
+//      darkModeButton.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
+//   }
  
+// const darkModeButton = document.querySelector('.dark-mode');
+
+// darkModeButton.addEventListener('click', darkMode);
+
+// ! Mode Dark //
+
+let theme = localStorage.getItem("theme");
 const darkModeButton = document.querySelector('.dark-mode');
 
-darkModeButton.addEventListener('click', darkMode);
+
+if (theme === "dark") {
+    document.body.classList.add("dark-mode");
+    darkModeButton.textContent = "☀️";  
+} else { darkModeButton.textContent = "🌙"; 
+}
+
+
+darkModeButton.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        darkModeButton.textContent = "☀️";  
+    } else {
+        localStorage.setItem("theme", "light");
+        darkModeButton.textContent = "🌙";  
+    }
+});
+
+
 
 //! like //
 
